1976ce30b4989dbbbcfb3683fa1ba5271712b27f
Francis Evans <francis.evans@gmail.com>
2025-02-10 18:00:01 +0000

    Added Test Project

---
+++ PDemo.Tests/Test1.cs
@@ -0,0 +1,19 @@
+﻿namespace PDemo.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void ShouldReturnTrue()
+    {
+        var response = true;
+        Assert.IsTrue(response);
+    }
+
+    [TestMethod]
+    public void ShouldReturnFalse()
+    {
+        var response = false;
+        Assert.IsFalse(response);
+    }
+}
